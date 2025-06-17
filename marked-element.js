f867ca45a22242d2f26b904db577ec5f44dbecbf
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-06-17 18:22:17 +0530

    fix marked

--- marked-element.js
+++ marked-element.js
@@ -9,7 +9,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 */
 import '@polymer/polymer/polymer-legacy.js';
 import './marked-import.js';
-const DOMPurify = require('dompurify');
 
 import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
 import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
@@ -308,18 +307,14 @@ Polymer({
       renderer: renderer,
       highlight: this._highlight.bind(this),
       breaks: this.breaks,
+      sanitize: this.sanitize,
+      sanitizer: this.sanitizer,
       pedantic: this.pedantic,
       smartypants: this.smartypants
     };
 
     dom(this._outputElement).innerHTML =
         marked(this.markdown, opts, this.callback);
-
-    let html = marked.parse(this.markdown, opts);
-    if (this.sanitize) {
-      html = DOMPurify.sanitize(html);
-    }
-    dom(this._outputElement).innerHTML = html;
     this.fire('marked-render-complete', {}, {composed: true});
   },
 
