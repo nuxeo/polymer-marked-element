1eaa3345caa502812e86087e276e6dfb137d66dd
rakeshkumar1019 <srakeshkumar1019@gmail.com>
2025-06-20 09:47:36 +0530

    align with open polymer-marked

--- marked-import.js
+++ marked-import.js
@@ -8,7 +8,7 @@ found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
 part of the polymer project is also subject to an additional IP rights grant
 found at http://polymer.github.io/PATENTS.txt
 */
-import { marked } from 'marked';
+import * as marked from 'marked/lib/marked.js';
 
 if (!window.marked) {
   // For webpack support for the Polymer 3 version created by the Polymer
